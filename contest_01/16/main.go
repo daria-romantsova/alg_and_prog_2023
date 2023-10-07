91e96c089fad9224107b8af3be5a564f75a66d01
daria-romantsova <145094789+daria-romantsova@users.noreply.github.com>
2023-10-07 14:51:52 +0300

    Create main.go
    main.go for contest_01, 16

---
+++ contest_01/16/main.go
@@ -0,0 +1,23 @@
+package main
+import "fmt"
+
+func main() {
+    var n, f int
+    fmt.Scanln(&n)
+    var array []int
+
+    for i := 0; i < n; i++ {
+        fmt.Scan(&f)
+        array = append(array, f)
+    }
+
+    fmt.Print(array[0], " ")
+
+    for i := 1; i+1 < n; i++ {
+        // Вычислить среднее знач тройки чисел
+        t := float64(array[i-1] + array[i] + array[i+1]) / 3
+        fmt.Print(t, " ")
+    }
+
+    fmt.Print(array[n-1])
+}
