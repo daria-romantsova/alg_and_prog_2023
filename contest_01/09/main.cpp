b11e0d1262d63f5a7ac0d52a1ea87cb7eef8bdae
daria-romantsova <145094789+daria-romantsova@users.noreply.github.com>
2023-10-07 12:17:39 +0300

    Create main.cpp
    main.cpp for contest_01, 09

---
+++ contest_01/09/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    float a; float b; float c;
+    std::cin >> a>>b;
+    c = sqrt((a*a)+(b*b));
+    std::cout << c << std::endl;
+}
