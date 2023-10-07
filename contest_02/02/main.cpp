3a7c9738ae9c208f49275f9bdc6ea0f9d809982f
daria-romantsova <145094789+daria-romantsova@users.noreply.github.com>
2023-10-07 19:07:28 +0300

    Update main.cpp

--- contest_02/02/main.cpp
+++ contest_02/02/main.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
 
+bool is_can_exit_from_maze(std::vector<std::string> maze, int row, int col) {
+    if (maze[row][col] == '.') return false;
+    if (maze[row][col] == '#') return false;
+    if (maze[row][col] == 'E') return true;
+    
+    maze[row][col] = '.';
+    
+    return is_can_exit_from_maze(maze, row + 1, col) ||
+           is_can_exit_from_maze(maze, row - 1, col) ||
+           is_can_exit_from_maze(maze, row, col + 1) ||
+           is_can_exit_from_maze(maze, row, col - 1);
+}
+
+int main(){
+    int row, col, rows, cols;
+    std::cin >> row >> col >> rows >> cols;
+    std::cin.ignore(1); // Игнорируем перевод строки, застрявший в потоке ввода
+    
+    std::vector<std::string> maze(rows);
+    for(auto& line : maze) std::getline(std::cin, line);
+    
+    std::cout << (is_can_exit_from_maze(maze, row, col) ? "YES" : "NO") << std::endl;
+}
