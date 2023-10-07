47ab149e783fe78a1fe6029276e313bac8aab9f7
daria-romantsova <145094789+daria-romantsova@users.noreply.github.com>
2023-10-07 16:33:37 +0300

    Update main.cpp

--- contest_02/01/main.cpp
+++ contest_02/01/main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
 
+bool is_prime(int number) {
+    if (number <= 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= number; i++) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    int number;
+    std::cin >> number;
+    std::cout << (is_prime(number) ? "YES" : "NO") << std::endl;
+}
